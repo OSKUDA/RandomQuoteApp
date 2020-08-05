9ac79134d1cf6ce1a695899c23c24dfda685ca64
Oskar Krishna Shrestha <oscarstha4770181@gmail.com>
2020-08-05 22:43:47 +0530

    added some more quotes

--- app/src/main/java/io/github/oskuda/randomquotes/MainActivity.java
+++ app/src/main/java/io/github/oskuda/randomquotes/MainActivity.java
@@ -28,11 +28,24 @@ public class MainActivity extends AppCompatActivity {
             "-- Alan J. Perlis (Epigrams in programming)",
     "Don't worry about what anybody else is going to do. The best way to\n" +
             "predict the future is to invent it.\n" +
-            "-- Alan Kay\n" +
-            "\n" +
+            "-- Alan Kay\n",
             "Premature optimization is the root of all evil (or at least most of it)\n" +
             "in programming.\n" +
-            "-- Donald Knuth"};
+            "-- Donald Knuth"
+    ,"A little learning is a dangerous thing.\n" +
+            "-- Alexander Pope"
+            ,"Computer science education cannot make anybody an expert programmer any\n" +
+            "more than studying brushes and pigment can make somebody an expert\n" +
+            "painter.\n" +
+            "-- Eric Raymond"
+    ,"Students should be evaluated on how well they can achieve the goals they\n" +
+            "strived to achieve within a realistic context. Students need to learn to\n" +
+            "do things, not know things.\n" +
+            "-- Roger Schank, Engines for Education"
+    ,"La tactique, c'est ce que vous faites quand il y a quelque chose à\n" +
+            "faire; la stratégie, c'est ce que vous faites quand il n'y a rien à\n" +
+            "faire.\n" +
+            "-- Xavier Tartacover"};
 
     private int randomNum=0;
 
